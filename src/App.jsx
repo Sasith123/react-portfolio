7f3f7f151527fbce61d74712476b5837933d81de
Sasith123 <sasith857@gmail.com>
2025-05-19 21:18:46 +0530

    Update App.jsx
    corrected imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import NavBar from './components/navbar';
-import Hero from './components/hero';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 
 function App() {
 
